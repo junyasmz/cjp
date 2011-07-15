86e73424326cd81bdc29fed1bc5fba5fa3ffd67a
Junya Nobuhara <Junya.smz@gmail.com>
2011-07-15 11:24:15 +0900

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cjp::Application.config.secret_token = '3e3190d3f9b08fbe877f163e9bd2c251d702bcd8f5389499393ab9f40a17163020dc6c73f97f9b4babe8ab0b5ba7a3eee23fe1f6f1a01003837a76c556d91b0d'
